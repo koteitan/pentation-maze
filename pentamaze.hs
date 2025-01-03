0d0ad61896c2cf4813be6a640c1b70b586569907
koteitan <ukaibutton@mac.com>
2025-01-04 03:30:08 +0900

    add mul3

--- pentamaze.hs
+++ pentamaze.hs
@@ -1,6 +1,8 @@
 import System.Environment (getArgs)
 import Data.List (uncons)
 
+-- penta(a,0,0,0,0) = (0,b,0,0,0)
+--   b = pentation function of a
 penta :: (Int ,Int ,Int ,Int ,Int ) ->      (Int ,Int ,Int ,Int ,Int )
 penta    ( 0  , b  , c  , d  , e  ) =       ( 0  , b  , c  , d  , e  )
 penta    ( a  , 0  , 0  , 0  , 0  ) = penta ( a-1, 2  , 0  , 0  , 0  )
@@ -16,11 +18,25 @@ testpenta x = do
   let (a, b, c, d, e) = penta (x, 0, 0, 0, 0)
   print (a, b, c, d, e)
 
+-- mul3(0,x,0) = (0,0,3x)
+-- by only inc/dec
+mul3 :: (Int ,Int ,Int ) ->     (Int ,Int  ,Int )
+mul3    ( 0  , 0  , y  ) =      ( 0  , 0   , y  )
+mul3    ( 0  , x  , y  ) = mul3 ( 1  , x-1 , y  )
+mul3    ( 1  , x  , y  ) = mul3 ( 2  , x   , y+1)
+mul3    ( 2  , x  , y  ) = mul3 ( 3  , x   , y+1)
+mul3    ( 3  , x  , y  ) = mul3 ( 0  , x   , y+1)
+
+testmul3 :: Int -> IO ()
+testmul3 x = do
+  let (a, b, c) = mul3 (0, x, 0)
+  print (a, b, c)
+
 main :: IO ()
 main = do
   args <- getArgs
   case uncons args of
-    Just (xStr, _) -> testpenta (read xStr :: Int)
+    Just (xStr, _) -> testmul3(read xStr :: Int)
     Nothing        -> loop                        
 
 loop :: IO ()
